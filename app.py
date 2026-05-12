de5aa809af6bd11cc4c8540972e6f3e8729bd544
wrifto <wrifto.contact@gmail.com>
2026-05-13 01:40:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -222,4 +222,5 @@ def create_app() -> Flask:
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True) 
+    web: python -m gunicorn app:app --bind 0.0.0.0:$PORT
